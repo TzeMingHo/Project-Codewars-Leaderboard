79098f639285f7060c1f357d6381f47fbd4521d0
TzeMingHo <tzemingho@gmail.com>
2026-01-28 17:16:24 +0000

    added two mock tests for username

--- index.mjs
+++ index.mjs
@@ -1,3 +1,11 @@
 export function makeFetchRequest() {
   return fetch("https://example.com/test");
 }
+
+export function invalidUserNameRequest() {
+  return fetch("https://www.codewars.com/api/v1/users/invalidUserName");
+}
+
+export function emptyUserNameRequest() {
+  return fetch("https://www.codewars.com/api/v1/users/ ");
+}
